ebee9bdea37344562c7b0d90c642d3b2327613ea
Victor <dallevictor@gmail.com>
2023-11-01 00:29:24 +0100

    formatage du fichier

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {logWrite.accessLogFile(req, res, next);});
+app.use((req, res, next) => {
+    logWrite.accessLogFile(req, res, next);
+});
 
 app.get('/', (req, res) => {
     res.send("I'm online stepbro !");
