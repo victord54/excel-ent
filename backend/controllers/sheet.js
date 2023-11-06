a0142d00ccd663c9712abba72353a03b86214e9b
Victor <dallevictor@gmail.com>
2023-11-06 21:01:38 +0100

    ajout validation des données reçues pour la création d'une feuille

--- backend/controllers/sheet.js
+++ backend/controllers/sheet.js
@@ -2,7 +2,7 @@ import { commitTransaction, rollbackTransaction } from '../utils/database.js';
 
 import {getAll as getAllSheets, getOne as getOneSheet} from '../models/sht_sheet_dql.js';
 import {create as createSheet} from '../models/sht_sheet_dml.js';
-import { SheetNotFoundError } from '../utils/error.js';
+import { MissingParameterError, SheetNotFoundError } from '../utils/error.js';
 
 /**
  * Fetch all sheets
@@ -44,6 +44,10 @@ export async function getOne(req, res, next) {
 export async function create(req, res, next) {
     const {sht_idtusr, sht_name, sht_data, sht_sharing, sht_uuid} = req.body;
     try {
+        // validation des données reçues
+        if (!sht_idtusr || !sht_name || !sht_data || !sht_sharing || !sht_uuid) {
+            throw new MissingParameterError('Missing parameters');
+        }
         const sheet = await createSheet({sht_idtusr, sht_name, sht_data, sht_sharing, sht_uuid});
         return res.status(200).json(sheet);
     } catch (error) {
