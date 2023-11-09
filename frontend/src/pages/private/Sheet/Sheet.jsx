a08582ec54d61d7578216bf78c569a587a7e584b
clairekth <claire.kurth@orange.fr>
2023-11-09 22:11:42 +0100

    Modification editing cells (en mieux et + simple)

--- frontend/src/pages/private/Sheet/Sheet.jsx
+++ frontend/src/pages/private/Sheet/Sheet.jsx
@@ -6,18 +6,12 @@ export default function Sheet() {
     let { uuidSheet} = useParams();
     const numberOfRows = 100;
     const numberOfColumns = 30;
-    const [isEditing, setIsEditing] = useState(false);
-    const [selectedCell, setSelectedCell] = useState(null);
-    const inputRef = useRef(null);
 
     const nameRows = Array.from( {length: numberOfColumns}, (_,index) => index + 1);
     const nameColums = generateNameColumns();
 
     const [cells, setCells] = useState([]);
 
-    let timer = 0;
-    let delay = 200;
-    let prevent = false;
 
     function generateNameColumns(){
         const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -33,61 +27,31 @@ export default function Sheet() {
         return res;
     }
 
-    /** Fonction qui permet de gérer le click */
-    function handleClick(event, cellKey){
-        event.preventDefault();
-        timer = setTimeout(function() {
-            if (!prevent) {
-                editingCell(cellKey);
-            }
-            prevent = false;
-        }, delay);
-    }
-
-    /** Fonction qui permet de gérer le double click */
-    function handleDoubleClick(event, cellKey){
-        event.preventDefault();
-        clearTimeout(timer);
-        prevent = true;
-        editingCell(cellKey);
-    }
-
-    function editingCell(cellKey){
-        setSelectedCell(cellKey);
-    }
 
     function handleInputChange(event, cellKey){
         const updatedCells = {...cells};
-        updatedCells[cellKey] = event.target.value;
+        updatedCells[cellKey] = event.target.innerText;
         setCells(updatedCells);
+        console.log(cells);
     }
 
-    function handleCellKeyDown(event){
+    function saveSheet(){
+        console.log('save');
+        console.log(cells);
+    }   
+
+    function handleEnter(event){
         if (event.key === 'Enter'){
-            setSelectedCell(null);
+            event.preventDefault();
+            event.target.blur();
         }
     }
 
-    useEffect(() => {
-        if (inputRef.current){
-            inputRef.current.focus();
-        } else {
-            inputRef.current.blur();
-        }
-    }, [selectedCell]);
 
     return (
         <>
+        <button onClick={saveSheet}>Save</button>
             <div className="sht-container-all">
-                <div><input 
-                    type='Text'
-                    value={cells[selectedCell] || ''}
-                    onChange={(event) => handleInputChange(event, selectedCell)}
-                    onKeyDown={(event) => handleCellKeyDown(event)}
-                    {...(selectedCell ? {disabled : false} : {disabled : true})}
-                    ref={inputRef}
-                    ></input>
-                </div>
                 <div className="sht-container-tab">
                     <table className='sht-table'>
                         <thead>
@@ -105,11 +69,10 @@ export default function Sheet() {
                                     <td key={nameRow}>{nameRow}</td>
                                     {nameColums.map(nameCol => (
                                         <td key={nameCol + '_' + nameRow} 
-                                        onClick={(event) => handleClick(event, nameCol + '_' + nameRow)} 
-                                        onDoubleClick={(event) => handleDoubleClick(event,nameCol + '_' + nameRow)}
-                                        className={selectedCell === nameCol + '_' + nameRow ? 'selected-cell' : ''}
+                                        contentEditable
+                                        onInput={(event) => handleInputChange(event, nameCol + '_' + nameRow)}
+                                        onKeyDown={(event) => handleEnter(event)}
                                         >
-                                            {cells[nameCol+'_'+nameRow]}
                                         </td>
                                     )
                                     )}
